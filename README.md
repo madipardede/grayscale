53d1ab1f7b77a9a4de4d44e2db0a8ecce60ae2e6
Ikhsan Fadly <ihsanfadly@gmail.com>
2021-01-09 02:57:54 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # grayscale
+
+#Tugas UAS
+#Image Processing
+
+merubah gambar RGB menjadi grayscale dengan java code
+
+for use
+follow the instruction
+1. open projek ini lewat netbeans
+2. run project via netbeans IDE
+3. pilih gambar RGB original
+4. klik proses dan tunggu sampai selesai
+
+to explain rgb logic to grayscale, inside the code is marked with a comment tag
+
+thanks
+madipardede
---
+++ src/grayscale/Grayscale.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grayscale;
+
+/**
+ *
+ * @author pardede
+ */
+public class Grayscale {
+    private static final MainFrame MAIN_FRAME = new MainFrame();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MAIN_FRAME.setVisible(true);
+    }
+    
+}
